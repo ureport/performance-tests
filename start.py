25e7aad75758c76867b79954bbd1eaaf3ada30c7
Felix Kitaka <webash.uganda@gmail.com>
2014-02-18 14:03:17 +0000

    [MJ,RL,FM] Implemented parallel message producers.

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 #create several messages with unique contact numbers
 import re
 import subprocess
+import threading
 import time
 import requests
 import numpy
@@ -37,27 +38,53 @@ def generate_report(message_stats):
     print "99th percentile: %f sec" % numpy.percentile(results, 99)
 
 def parse_data_from_line(line):
-    gex = re.compile("destination_addr=(\d+) .* timestamp=(\d+)")
+    gex = re.compile("destination_addr=(\d+) .*Welcome to U.* timestamp=(\d+)")
     match = gex.search(line)
     if match:
         return match.groups()
     return None
 
+
+def send_chunked_message(chunk, message_stats):
+    for number in chunk:
+        time_sent, response = send_message(number, "join")
+        message_stats[number]["start"] = time_sent
+        message_stats[number]["response"] = response.status_code
+
+def send_messages(message_stats, chunk_size=1):
+    chunks = [message_stats.keys()[i::chunk_size] for i in xrange(chunk_size)]
+    print chunks
+
+    for chunk in chunks:
+        t = threading.Thread(target=send_chunked_message, args=(chunk, message_stats))
+        t.start()
+
+def create_message_stats(numbers):
+    message_stats = {}
+
+    for number in numbers:
+        message_stats[number] = {}
+
+    return message_stats
+
 ### MAIN
 
+# configuration
 logfile = "/vagrant/python-smpp/smpp/smsc.log"
 timeout = 300
 number_of_phone_numbers = 1000
-counter = 0
+number_of_parallel_message_producers = 4
 
 numbers = generate_numbers(number_of_phone_numbers)
-message_stats = {}
-for number in numbers:
-    time_sent, response = send_message(number, "join")
-    message_stats[number] = {"start": time_sent, "response": response.status_code}
+message_stats = create_message_stats(numbers)
 
+send_message_thread = threading.Thread(target=send_messages, args=(message_stats,number_of_parallel_message_producers,))
+send_message_thread.start()
+
+# extract responses from smsc log
 f = subprocess.Popen(['tail', '-F', logfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+counter = 0
 start_time = time.time()
 while True:
     line = f.stdout.readline()
