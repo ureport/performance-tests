265785c609605f81fbe263cffa36ee94c8e72a80
vivitc <vivitc@gmail.com>
2014-03-13 12:15:16 +0300

    [Vivi] Changing the way that read input information.

--- statistics_for_individual_msgs.py
+++ statistics_for_individual_msgs.py
@@ -3,45 +3,42 @@ from statistic_helper import execute_command, get_diff, statistic_for, get_time_
 
 def run_test():
     """
-    Filter individual messages from db -> get id, connection_id and date
-    Steps
-    1.- Get send_message logs from the time that a big poll was sent out
-    2.- Filter the logs for individual send sms E.g.grep -i "INFO: SMS\[........\] SENT" messenger.log
-    3.- Get from db message detail where ids in the ids filter from logs and direction='O' and batch is NULL
-    -> this info in send_individuals.txt
+    We would need:
+    1.- Filter individual messages from db -> get id, connection_id and date
+    psql -U postgres -h localhost -d ureport -c "select id, connection_id, date
+    from rapidsms_httprouter_message where direction='O' and
+    id in ($MESSAGE_IDs THAT ARE NOT ASSOCIATED TO A POLL);" > individual_messages_dates.txt
+    2.- Get send_message logs from the time that a big poll was sent out. E.g. messenger.log
     """
-    line = 3
-    limit = 50
+    ini = 3
     time_diff_msg_db_to_send_out = []
 
+    with open('individual_messages_dates.txt') as response_data:
+        for index, line in enumerate(response_data):
+            if index >= ini:
+                times = getting_times_for(line)
+                time_diff_msg_db_to_send_out.append(get_diff(times['message_in_db'],times['sms_sent_out_time']))
 
-    while (line < limit):
-        times = getting_times_for_line(line)
-        time_diff_msg_db_to_send_out.append(get_diff(times['message_in_db'],times['sms_sent_out_time']))
-        line = line + 1
 
     statistic_for("Time that individual messages take to be sent out", time_diff_msg_db_to_send_out)
 
 def get_sent_out_time(message_id):
-    #  2014-03-03 16:00:03,597 [command] INFO: SMS[50751122] SENT
-    command_to_get_time = "grep -i %s send_individuals.txt" % message_id
+    #  2014-03-03 16:00:03,597 [command] INFO: SMS[$MESSAGE_ID] SENT
+    command_to_get_time = "grep -i %s messenger.log*" % message_id
     result = execute_command(command_to_get_time)
     regex = "(.*) \[command\] INFO: SMS(.*)"
     format_time = "%Y-%m-%d %H:%M:%S,%f"
     return get_time_from_result(result, regex, 1, format_time)
 
 
-def getting_times_for_line(line):
-    command_to_get_message_id = "head -n %d individual_messages_dates.txt | tail -1" % line
-    result = execute_command(command_to_get_message_id)
-    result_data= result.split('|')
+def getting_times_for(line):
+    data = line.split("|")
 
-    message_id = result_data[0].strip()
-    connection_id = result_data[1].strip()
-    date = result_data[2].strip().split('+')[0]
+    message_id = data[0].strip()
+    date = data[2].strip().split('+')[0]
 
     times = {}
-    times['message_in_db']=datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
+    times['message_in_db'] = datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
     times['sms_sent_out_time'] = get_sent_out_time(message_id)
     return times
 
