69a91f7af1d9c1faf277b80583f54501a5e3d272
vivitc <vivitc@gmail.com>
2014-03-13 11:14:52 +0300

    [Vivi] Refactoring methods duplicated from statistic helper.

--- statistics_for_individual_msgs.py
+++ statistics_for_individual_msgs.py
@@ -1,6 +1,5 @@
-import re
-import subprocess
 from datetime import datetime
+from statistic_helper import execute_command, get_diff, statistic_for, get_time_from_result
 
 def run_test():
     """
@@ -23,38 +22,13 @@ def run_test():
 
     statistic_for("Time that individual messages take to be sent out", time_diff_msg_db_to_send_out)
 
-
-def between(date, ini, end):
-    return date<end and date > ini
-
-def statistic_for(name, latency_request_list):
-    average_latency = get_average_latency(latency_request_list)
-    max_latency = max(latency_request_list)
-    min_latency = min(latency_request_list)
-    median_latency = median(latency_request_list)
-
-    print "Statistics for " + name
-    print latency_request_list
-    print "Total : %s" % str(len(latency_request_list))
-    print 'Average latency:' + str(average_latency)
-    print 'Max latency:' + str(max_latency)
-    print 'Min latency:' + str(min_latency)
-    print 'Median latency: ' + str(median_latency)
-
-
-def get_average_latency(latency_request_list):
-    return sum(latency_request_list) / len(latency_request_list)
-
-
 def get_sent_out_time(message_id):
     #  2014-03-03 16:00:03,597 [command] INFO: SMS[50751122] SENT
     command_to_get_time = "grep -i %s send_individuals.txt" % message_id
     result = execute_command(command_to_get_time)
-    result_time = re.search("(.*) \[command\] INFO: SMS(.*)", result, re.IGNORECASE)
-    result_time = result_time.group(1)
-
-    time = datetime.strptime(result_time, "%Y-%m-%d %H:%M:%S,%f")
-    return time
+    regex = "(.*) \[command\] INFO: SMS(.*)"
+    format_time = "%Y-%m-%d %H:%M:%S,%f"
+    return get_time_from_result(result, regex, 1, format_time)
 
 
 def getting_times_for_line(line):
@@ -70,25 +44,5 @@ def getting_times_for_line(line):
     times['message_in_db']=datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
     times['sms_sent_out_time'] = get_sent_out_time(message_id)
     return times
-
-def get_diff(start_time, end_time):
-    diff = (end_time - start_time)
-    return diff.total_seconds()
-
-def execute_command(command):
-    shell_response = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    lines = shell_response.stdout.readlines()
-    if len(lines) > 0:
-        return lines[0].strip()
-    else:
-        return None
-
-def not_empty(value):
-    return value is not None and value!=""
-
-def median(a):
-    ordered = sorted(a)
-    length = len(a)
-    return float((ordered[length/2] + ordered[-(length+1)/2]))/2
 
 run_test()
